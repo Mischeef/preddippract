ddb598e481bb4678a345158eaddf477427208911
Mischeef <gerpv1@mail.ru>
2024-12-07 19:23:45 +0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -126,3 +126,6 @@ class VAE(nn.Module):
         return x, mu, logvar
     # --- Функция потерь для VAE ---
 def vae_loss(recon_x, x, mu, logvar):
+    recon_loss = F.mse_loss(recon_x, x, reduction='sum')
+    kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return recon_loss + kl_loss
