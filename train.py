7542afbb63a448c249e28f91c871ae8751258494
Mischeef <gerpv1@mail.ru>
2024-12-19 18:30:09 +0300

    Update train.py
    добавление collate.fn для выравнивания размеров тензоров в батче

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 import trimesh
 from torch.utils.data import Dataset, DataLoader
 import torch
+from torch.nn.utils.rnn import pad_sequence
 from shap_e.models.download import load_model, load_config
 from shap_e.diffusion.gaussian_diffusion import diffusion_from_config
 import matplotlib.pyplot as plt
@@ -36,15 +37,22 @@ class ModelNet10Dataset(Dataset):
             vertices = self.transform(vertices)
         return vertices, category
 
+# Функция для коллатного батча с паддингом
+def collate_fn(batch):
+    vertices, categories = zip(*batch)
+    vertices_padded = pad_sequence([torch.tensor(v, dtype=torch.float32) for v in vertices], batch_first=True)
+    categories = torch.tensor(categories)
+    return vertices_padded, categories
+
 # Укажите абсолютный путь к папке ModelNet10
 modelnet10_path = 'c:/Users/gerpv/Desktop/predpp/preddippract/ModelNet10'
 
 # Пример использования
 train_dataset = ModelNet10Dataset(root_dir=modelnet10_path, mode='train')
-train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True, collate_fn=collate_fn)
 
 test_dataset = ModelNet10Dataset(root_dir=modelnet10_path, mode='test')
-test_dataloader = DataLoader(test_dataset, batch_size=4, shuffle=False)
+test_dataloader = DataLoader(test_dataset, batch_size=4, shuffle=False, collate_fn=collate_fn)
 
 # Настройка модели и оптимизатора
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -61,9 +69,8 @@ for epoch in range(num_epochs):
     model.train()
     epoch_loss = 0
     for vertices, categories in train_dataloader:
-        # Преобразование данных в формат, подходящий для модели
-        vertices = torch.tensor(vertices, dtype=torch.float32).to(device)
-        categories = torch.tensor(categories).to(device)
+        vertices = vertices.to(device)
+        categories = categories.to(device)
         optimizer.zero_grad()
         loss = diffusion.training_losses(model, vertices, categories)
         loss.backward()
